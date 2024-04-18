3209afdb445af7789296633761f573915c9d91ed
Prem Kolte <100456448+Premkolte@users.noreply.github.com>
2024-04-19 03:14:39 +0530

    Time: 79 ms (51.49%) | Memory: 70.3 MB (29.67%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -3,8 +3,8 @@ public:
     int maxSubArray(vector<int>& nums) {
         int sum=0;
         int maxi= INT_MIN;
-        for(auto i:nums){
-            sum +=i;
+        for(int i=0;i<nums.size();i++){
+            sum +=nums[i];
             maxi=max(maxi,sum);
             if(sum<0) sum=0;
         }
