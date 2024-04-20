8e085cabdf373807b302041e1086a27de5e0b354
Prem Kolte <100456448+Premkolte@users.noreply.github.com>
2024-04-20 12:41:19 +0530

    Time: 57 ms (97.11%) | Memory: 70.1 MB (67.26%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        // std::ios::sync_with_stdio(0);
-        // std::cin.tie(0);
-        int sum = 0;
-        int maxi = INT_MIN;
-        for (int i = 0; i < nums.size(); i++) {
-            sum += nums[i];
-
-            maxi = max(maxi, sum);
-
-            if (sum < 0)
-                sum = 0;
+        int sum=0;
+        int maxi= INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            sum +=nums[i];
+            maxi=max(maxi,sum);
+            if(sum<0) sum=0;
         }
-
         return maxi;
     }
 };
