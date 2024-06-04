d0461bad2bd9eea3726b50d7312716ef74f88a46
Prem Kolte <100456448+Premkolte@users.noreply.github.com>
2024-06-04 21:24:12 +0530

    Time: 5 ms (27.77%) | Memory: 11 MB (31.82%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -6,7 +6,7 @@ public:
         int k = m + n - 1;
         
         while (j >= 0) {
-            if (i >= 0 && nums1[i] > nums2[j]) {
+          if (i >= 0 && nums1[i] > nums2[j]) {
                 nums1[k--] = nums1[i--];
             } else {
                 nums1[k--] = nums2[j--];
