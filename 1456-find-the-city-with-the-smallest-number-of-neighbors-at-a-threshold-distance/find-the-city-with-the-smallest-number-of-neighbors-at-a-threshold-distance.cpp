9f1ccd589a4abfac909bc8b599d9d983d1c02917
Prem Kolte <100456448+Premkolte@users.noreply.github.com>
2024-07-26 13:16:25 +0530

    Time: 25 ms (67.65%) | Memory: 15.5 MB (79.53%) - LeetSync

---
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,57 @@
+#include <vector>
+#include <limits>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        // Initialize the distance matrix with infinity
+        vector<vector<int>> dist(n, vector<int>(n, numeric_limits<int>::max()));
+        
+        // Distance to itself is 0
+        for (int i = 0; i < n; ++i) {
+            dist[i][i] = 0;
+        }
+        
+        // Populate the distance matrix with the given edges
+        for (const auto& edge : edges) {
+            int u = edge[0], v = edge[1], w = edge[2];
+            dist[u][v] = w;
+            dist[v][u] = w;
+        }
+        
+        // Floyd-Warshall algorithm
+        for (int k = 0; k < n; ++k) {
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    if (dist[i][k] != numeric_limits<int>::max() && dist[k][j] != numeric_limits<int>::max()) {
+                        dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                    }
+                }
+            }
+        }
+        
+        // Find the city with the smallest number of reachable cities
+        // and if there is a tie, choose the city with the greatest number.
+        int minReachableCities = numeric_limits<int>::max();
+        int bestCity = -1;
+        
+        for (int i = 0; i < n; ++i) {
+            int reachableCities = 0;
+            for (int j = 0; j < n; ++j) {
+                if (dist[i][j] <= distanceThreshold) {
+                    reachableCities++;
+                }
+            }
+            
+            if (reachableCities <= minReachableCities) {
+                minReachableCities = reachableCities;
+                bestCity = i;
+            }
+        }
+        
+        return bestCity;
+    }
+};
