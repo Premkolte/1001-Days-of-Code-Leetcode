720bf048b32cc81269c2db41a3ee898124c86470
Prem Kolte <premkolte26@gmail.com>
2025-06-02 23:57:17 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (43.50%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings, int cnt = 0) {
+        int n = ratings.size();
+        vector<int> candies(n, 1);
+
+        for (int i = 1; i < n; i++) 
+            if (ratings[i] > ratings[i - 1])
+                candies[i] = candies[i - 1] + 1;
+        
+        for (int i = n - 1; i > 0; i--) {
+            if (ratings[i - 1] > ratings[i])
+                candies[i - 1] = max(candies[i] + 1, candies[i - 1]);
+            cnt += candies[i - 1];
+        }
+        return cnt + candies[n - 1];
+    }
+};
