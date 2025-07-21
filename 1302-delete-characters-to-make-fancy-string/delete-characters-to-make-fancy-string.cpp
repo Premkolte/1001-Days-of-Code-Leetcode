5acff95a271e22a216a4267d32fa3b8a7ce860dc
Prem Kolte <premkolte26@gmail.com>
2025-07-21 20:57:50 +0530

    Time: 30 ms (49.50%) | Memory: 44.4 MB (67.47%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
@@ -4,15 +4,26 @@ public:
         string ans = "";
         ans.push_back(s[0]);
         int n = s.size(),cnt = 1;
+        // for(int i=1;i<n;i++){
+        //     if(s[i] == ans.back()){
+        //         cnt++;
+        //         if(cnt < 3)ans.push_back(s[i]);
+        //     }
+        //     else{
+        //         cnt = 1;
+        //         ans.push_back(s[i]);
+        //     }
+        // }
+
         for(int i=1;i<n;i++){
-            if(s[i] == ans.back()){
+            if(s[i]==ans.back()){
                 cnt++;
-                if(cnt < 3)ans.push_back(s[i]);
+                if(cnt<3) ans.push_back(s[i]);
             }
             else{
-                cnt = 1;
+                cnt=1;
                 ans.push_back(s[i]);
-            }
+        }
         }
         return ans;
     }
