1865dcdd6bc8a01f0037afa7a4f76233021ff57c
Prem Kolte <premkolte26@gmail.com>
2025-07-31 02:28:08 +0530

    Time: 11 ms (6.70%) | Memory: 24 MB (16.99%) - LeetSync

---
+++ 155-min-stack/min-stack.cpp
@@ -0,0 +1,25 @@
+class MinStack {
+private:
+    vector<vector<int>> st;
+
+public:
+    MinStack() {
+        
+    }
+    void push(int val) {
+        int min_val = getMin();
+        if (st.empty() || min_val > val) {
+            min_val = val;
+        }
+        st.push_back({val, min_val});        
+    }
+    void pop() {
+        st.pop_back();
+    }
+    int top() {
+        return st.empty() ? -1 : st.back()[0];
+    }
+    int getMin() {
+        return st.empty() ? -1 : st.back()[1]; 
+    }
+};
