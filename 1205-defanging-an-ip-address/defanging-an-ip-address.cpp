5edae8ddb46e124fee78c9a1b5348482bbab4820
Prem Kolte <premkolte26@gmail.com>
2025-08-17 02:18:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (76.45%) - LeetSync

--- 1205-defanging-an-ip-address/defanging-an-ip-address.cpp
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.cpp
@@ -4,9 +4,7 @@ public:
         string s="";
         for(char &p : address){
             if(p=='.'){
-                s.push_back('[');
-                s.push_back('.');
-                s.push_back(']');
+                s.append("[.]");
             }
             else s.push_back(p);
         }
