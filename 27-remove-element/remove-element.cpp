404d27e35a2adc03c91d344989216d5b416c7ee9
Prem Kolte <premkolte26@gmail.com>
2026-07-08 13:14:51 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (48.62%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,18 +1,33 @@
+// class Solution {
+// public:
+//     int removeElement(vector<int>& nums, int val) {
+//         vector<int> a;
+//         for(auto i:nums){
+//             if(i!=val){
+//                 a.push_back(i);
+
+//             }
+//         }
+
+//         for(int i=0;i<a.size();i++){
+//             nums[i]=a[i];
+//         }
+
+//         return a.size();
+//     }
+// };
+
+
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        vector<int> a;
-        for(auto i:nums){
-            if(i!=val){
-                a.push_back(i);
-
+        int index = 0;
+        for(int i = 0; i< nums.size(); i++){
+            if(nums[i] != val){
+                nums[index] = nums[i];
+                index++;
             }
         }
-
-        for(int i=0;i<a.size();i++){
-            nums[i]=a[i];
-        }
-
-        return a.size();
+        return index;
     }
 };
