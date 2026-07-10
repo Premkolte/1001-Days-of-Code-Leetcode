22176cc27acafb2f2ad3d3f6b7c9c8c8be7c68a5
Prem Kolte <premkolte26@gmail.com>
2026-07-10 12:54:22 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (98.68%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int maxReach = 0;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (i > maxReach) return false;
-            maxReach = max(maxReach, i + nums[i]);
+        // int maxReach = 0;
+        // for (int i = 0; i < nums.size(); ++i) {
+        //     if (i > maxReach) return false;
+        //     maxReach = max(maxReach, i + nums[i]);
+        // }
+        // return true;
+
+        int n = nums.size();
+        int flag=n-1;
+        for(int i=n-2;i>=0;i--){
+            if(i+nums[i]>=flag){
+                flag = i;
+            }
         }
-        return true;
+
+        return flag==0;
     }
 };
