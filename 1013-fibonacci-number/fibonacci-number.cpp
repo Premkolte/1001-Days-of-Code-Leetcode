f83c9a5f2f96e68cc86219a11477256ba49537c6
Prem Kolte <premkolte26@gmail.com>
2026-09-12 19:42:44 +0530

    Time: 14 ms (22.41%) | Memory: 7.8 MB (65.30%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,22 +1,9 @@
 class Solution {
 public:
-//0 1 1 2 3 5 8 11
-    int fibbo(int n)
-{
-    int a = 0, b = 1, c, i;
-    if (n == 0)
-        return a;
-    for (i = 2; i <= n; i++) {
-        c = a + b;
-        a = b;
-        b = c;
-    }
-    return b;
-}
-
     int fib(int n) {
         if(n==0) return 0;
-        else if(n==1) return 1;
-        else return fibbo(n-1) + fibbo(n-2);
+        if(n==1) return 1;
+
+        return fib(n-1) + fib(n-2);
     }
 };
