74c4c7631f199a9ffbf136155450e73c5b8b8b41
Prem Kolte <premkolte26@gmail.com>
2026-09-13 11:47:55 +0530

    Time: 3 ms (64.51%) | Memory: 7.8 MB (38.02%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,9 +1,34 @@
 class Solution {
 public:
+    unordered_map<int,int> dp;
     int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
+        // if(n==0) return 0;
+        // if(n==1) return 1;
 
-        return fib(n-1) + fib(n-2);
+        // if(dp.find(n) != dp.end()){
+        //     return dp[n];
+        // }
+
+        // int a1=fib(n-1);
+        // int a2=fib(n-2);
+
+        // int ans = a1+a2;
+        // dp[n] = ans;
+
+        // return ans;
+
+        if(n==0 || n==1) return n;
+
+        int prev=1;
+        int prevprev=0;
+        int ans=0;
+
+        for(int i=2;i<=n;i++){
+            ans=prev+prevprev;
+            prevprev=prev;
+            prev = ans;
+        }
+
+        return ans;
     }
 };
