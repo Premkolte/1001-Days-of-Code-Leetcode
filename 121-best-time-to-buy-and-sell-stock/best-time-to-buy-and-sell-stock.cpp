085e62bc0e597bfea11c9e30a0d4d86bc812654f
Prem Kolte <premkolte26@gmail.com>
2026-09-16 15:59:11 +0530

    Time: 188 ms (6.53%) | Memory: 169.2 MB (5.23%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
-    int maxProfit(std::vector<int>& prices) {
-        int buy = prices[0];
-        int profit = 0;
-        for (int i = 1; i < prices.size(); i++) {
-            if (prices[i] < buy) {
-                buy = prices[i];
-            } else if (prices[i] - buy > profit) {
-                profit = prices[i] - buy;
-            }
+    int bns(vector<int>& prices,int n, int i, int k, vector<vector<int>> &dp){
+        if(i==n) return 0;
+
+        if(k==0) return 0;
+
+        if(dp[i][k] != -1) return dp[i][k];
+        if(k==2){
+            int c1 = bns(prices, n, i+1, k-1, dp) - prices[i];
+            int c2 = bns(prices, n, i+1, k, dp);
+
+            return dp[i][k] =  max(c1,c2);
         }
-        return profit;
+        else{
+            int c1 = bns(prices, n, i+1, k-1,dp) + prices[i];
+            int c2 = bns(prices, n, i+1, k,dp);
+
+            return dp[i][k] = max(c1,c2);
+
+        }
+
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int k=2;
+
+        vector<vector<int>> dp(n+1, vector<int> (k+1,-1));
+        return bns(prices, n, 0, k, dp);
     }
 };
