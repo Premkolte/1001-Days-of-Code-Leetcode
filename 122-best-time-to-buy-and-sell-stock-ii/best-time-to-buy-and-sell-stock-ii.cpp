c3cd92b78fa1fb1fcba985ba99bf7e6a04b86ebb
Prem Kolte <premkolte26@gmail.com>
2026-09-16 16:24:35 +0530

    Time: 11 ms (29.33%) | Memory: 28 MB (6.41%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -7,13 +7,13 @@ public:
 
         if(dp[i][k] != -1) return dp[i][k];
         if(k==2){
-            int c1 = bns(prices, n, i+1, k-1, dp) - prices[i];
+            int c1 = bns(prices, n, i+1, k-1, dp) - prices[i]; //buy
             int c2 = bns(prices, n, i+1, k, dp);
 
             return dp[i][k] =  max(c1,c2);
         }
         else{
-            int c1 = bns(prices, n, i+1, 2,dp) + prices[i];
+            int c1 = bns(prices, n, i+1, 2 ,dp) + prices[i]; // sell and updated k=2
             int c2 = bns(prices, n, i+1, k,dp);
 
             return dp[i][k] = max(c1,c2);
