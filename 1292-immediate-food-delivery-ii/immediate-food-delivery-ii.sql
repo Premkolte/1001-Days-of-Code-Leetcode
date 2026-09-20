9dbddc6af4067329feb7caf99e673b2efa01f2ff
Prem Kolte <premkolte26@gmail.com>
2026-09-20 17:19:31 +0530

    Time: 912 ms (16.05%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,9 @@
+select 
+    round(sum(if(order_date = customer_pref_delivery_date,1,0)) * 100 / count(distinct customer_id),2) as immediate_percentage
+
+from Delivery 
+where (customer_id, order_date) in (
+    select customer_id, min(order_date) as firstorderdate
+    from Delivery
+    group by customer_id    
+)
