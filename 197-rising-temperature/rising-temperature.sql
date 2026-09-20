7d62e7f440d06d45a2334eed9b1cc9e213045197
Prem Kolte <premkolte26@gmail.com>
2026-09-20 19:21:05 +0530

    Time: 501 ms (68.60%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
-select w1.id from Weather w1
+select w1.id
+from Weather w1
 join Weather w2
 on datediff(w1.recordDate, w2.recordDate) = 1
-Where w1.temperature > w2.temperature;
+where w1.temperature > w2.temperature
