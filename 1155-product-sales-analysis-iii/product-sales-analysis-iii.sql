20f224a700525ae69e343ce2d010dac735a56d9e
Prem Kolte <premkolte26@gmail.com>
2026-09-21 12:00:11 +0530

    Time: 760 ms (59.30%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,12 +1,9 @@
 # Write your MySQL query statement below
-select 
-    product_id, 
-    year as first_year,
-    quantity, 
-    price
+select product_id, year as first_year, quantity, price
 from Sales
 where (product_id, year) in (
     select product_id, min(year)
     from Sales
     group by product_id
+    
 )
