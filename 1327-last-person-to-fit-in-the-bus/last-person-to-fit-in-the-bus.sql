2de8806c07c0d9cfe22a1946bcfd3ea707df8c50
Prem Kolte <premkolte26@gmail.com>
2026-09-21 16:27:50 +0530

    Time: 2021 ms (8.62%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select q1.person_name
+from Queue q1
+inner join Queue q2
+on q1.turn >= q2.turn
+group by q1.turn 
+having sum(q2.weight) <=1000
+order by sum(q2.weight) desc
+limit 1
